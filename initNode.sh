028dc6a4e937ff5c06b59f104bee9a975a023de9
yu-antonyuk <83603871+yu-antonyuk@users.noreply.github.com>
2024-05-15 17:27:32 +0300

    Update initNode.sh

--- initNode.sh
+++ initNode.sh
@@ -1,30 +1,29 @@
+
 #!/bin/bash
-KEY
-# Update package list and upgrade packages
-apt-get update && apt-get install -y \
-    ca-certificates \
-    && rm -rf /var/lib/apt/lists/*
 
-# Prompt for the $KEY
-read -p "Enter the value for KEY: " KEY
+# Update and install necessary packages
+apt-get update && apt-get install -y ca-certificates
 
-# Download Geth
+# Download Geth binary
 wget https://github.com/LarissaBlockchain/Core/releases/download/v1.12.0/geth-ubuntu-x86_64
 
-# Create a script to run Geth
+# Prompt the user for the value of KEY
+read -p "Enter your KEY value: " KEY
+
+# Create the runnode.sh script
 cat <<EOF > runnode.sh
 #!/bin/bash
 
 # Set permissions for Geth
 chmod +x geth-ubuntu-x86_64
 
-
-# Run Geth with the provided $KEY
-./geth-ubuntu-x86_64 --larissa.node=1 -larissa.node.user.key="\$KEY"
+# Run Geth with the provided \$KEY
+./geth-ubuntu-x86_64 --larissa.node=1 -larissa.node.user.key=$KEY
 EOF
 
-# Set permissions for the script
+echo "File runnode.sh saved"
+# Make runnode.sh executable
 chmod +x runnode.sh
 
-# Edit crontab to run the script at reboot
-(crontab -l ; echo "@reboot sleep 60 && cd /path/to/script && ./runnode.sh") | crontab -
+echo "Add to cron:"
+(crontab -l ; echo "@reboot sleep 60 && sh runnode.sh") | crontab -
